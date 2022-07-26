d10489c9bc241b95cb076b448984dbf53649c6a1
Rafael Santos <rafaelps15@hotmail.com>
2022-07-26 16:42:31 -0300

    Mais exemplos atualizados

---
+++ Js - Desestruturação/script.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Paulo",
+    sobrenome: "Brandão",
+    links: {
+        instagram: "link1",
+        linkedin: "link2",
+        github: "link3",
+    }
+}
+
+//desestruturação
+const nome = pessoa.nome
+console.log(nome);
